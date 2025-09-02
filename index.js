26a610b50f5477e777a8e473bec8359c068ac195
Waltdev29 <waltdev29@gmail.com>
2025-09-02 14:37:06 +0900

    chp04 Button

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Library from "./chp03/Library";
-import './chp03/Book.css'
+import ConfirmDialog from './chp04/ConfirmDialog'
+import './chp04/Button.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Library/>
+        <ConfirmDialog/>
     </React.StrictMode>
 );
 

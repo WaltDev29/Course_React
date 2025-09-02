de8d648fc822f55d1f05b3373fc804f011783668
Waltdev29 <waltdev29@gmail.com>
2025-09-02 14:56:20 +0900

    CSS Module로 리팩토링

--- chp04/Button.jsx
+++ chp04/Button.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from './Button.module.css';
 
 function Button(props) {
     return (
-        <button className={`bg-${props.color}`}>
+        <button className={`${styles[`bg-${props.color}`]} ${styles.button}`}>
             <b>{props.children}</b>
         </button>
     )
--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ConfirmDialog from './chp04/ConfirmDialog'
-import './chp04/Button.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

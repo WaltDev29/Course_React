8b8f54adc916bb33fdcd99eaff77401bf3751693
Waltdev29 <waltdev29@gmail.com>
2025-09-09 16:31:41 +0900

    component 연습

---
+++ chp05/exam02/Book.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrap = styled.div`
+    margin: 8px;
+    padding: 8px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 350px;
+    border: 1px solid black;
+    border-radius: 16px;
+    background-color: #d2c8ed;
+    aspect-ratio: 4/3;
+    box-sizing: border-box;
+    box-shadow: 0 2px 4px rgba(0,0,0,0.05);
+`;
+
+const BookImg = styled.img`
+    width: 100%;
+    max-width: 250px;
+    height: auto;
+    flex-grow: 1;
+    border-radius: 8px;
+    object-fit: cover;
+`;
+
+const BookContent = styled.div`
+    margin-top: 16px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`;
+
+const Title = styled.span`
+    color: black;
+    font-size: 18px;
+    font-weight: bold;
+`;
+
+const Author = styled.span`
+    color: #555555;
+    font-size: 14px;
+    font-weight: 4px;
+`;
+
+function Book(props) {
+    return (
+        <Wrap className="wrapper">
+            <div>
+                <BookImg className="image"
+                    src={props.image}
+                    alt={props.title}/>
+            </div>
+            <BookContent className="contentContainer">
+                <Title>{props.title}</Title>
+                <Author>{props.author}</Author>
+            </BookContent>
+        </Wrap>
+    )
+}
+
+export default Book;
--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Comment from './chp05/Comment'
+import BookList from "./chp05/exam02/BookList";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
     root.render(
         <React.StrictMode>
-            <Comment/>
+            <BookList/>
         </React.StrictMode>
     );
 

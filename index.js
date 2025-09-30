73a16225cc2182dd9bbcb6cd5eef3ede9768a9a1
Waltdev29 <waltdev29@gmail.com>
2025-09-30 16:20:03 +0900

    이벤트 핸들링 연습 (버튼 disabled)

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Assign from "./chp08/Assign";
+import ConfirmButton from "./chp08/ConfirmButton";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
     root.render(
         <React.StrictMode>
-            <Assign/>
+            <ConfirmButton/>
         </React.StrictMode>
     );
 

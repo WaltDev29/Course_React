c64d7efe8763c86d02f28844c21796b6021b2a8c
Waltdev29 <waltdev29@gmail.com>
2025-09-30 16:00:43 +0900

    이벤트 핸들링 연습 (state 활용)

--- chp08/Assign.jsx
+++ chp08/Assign.jsx
@@ -1,17 +1,20 @@
 import React, {useState} from "react";
 
 function Assign(props) {
-    const[text, setText] = useState("")
-
-    function handleAlert(event) {
-        alert(text);
-    }
+    const[input, setInput] = useState("");
+    const[text, setText] = useState("");
 
     return (
         <div>
             <p>입력한 텍스트를 알림으로 출력합니다.</p>
-            <input type="text" onChange={(event) => setText(event.target.value)}/>
-            <button onClick={handleAlert}>제출</button>
+            <input type="text" onChange={(event) => setInput(event.target.value)}/>
+            <button onClick={() => {
+                alert(input);
+                setText(input);
+            }}>제출</button>
+            <div>
+                <h2 id="result">입력된 내용 : {text}</h2>
+            </div>
         </div>
     )
 }

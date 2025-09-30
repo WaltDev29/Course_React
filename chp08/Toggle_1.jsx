94ed3d8f3415d6104bfe90eb65f05544a2653c23
Waltdev29 <waltdev29@gmail.com>
2025-09-30 14:23:06 +0900

    8장 이벤트 핸들링

---
+++ chp08/Toggle_1.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+    font-size: 30px;
+    padding: 10px;
+    border-radius: 10px;
+    cursor: pointer;
+`;
+
+class Toggle_1 extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {isToggleOn: true};
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(preState => ({
+            isToggleOn: !preState.isToggleOn
+        }))
+    }
+
+    render() {
+        return (
+            <div>
+                <Button onClick={this.handleClick}>
+                    {this.state.isToggleOn ? "On" : "Off"}
+                </Button>
+            </div>
+        )
+    }
+}
+
+export default Toggle_1;
--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Accommodate from "./chp07/Accommodate";
+import Toggle_1 from "./chp08/Toggle_1"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
     root.render(
         <React.StrictMode>
-            <Accommodate/>
+            <Toggle_1 />
         </React.StrictMode>
     );
 

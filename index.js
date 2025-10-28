7bd15d004d9c62f4456c27de1290d49787a23001
Waltdev29 <waltdev29@gmail.com>
2025-10-28 14:07:39 +0900

    10장 List

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import NumberList from "./chp10/NumberList";
+import AppNumberList from "./chp10/AppNumberList";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <NumberList numbers={[1,2,3,4,5]}/>
+        <AppNumberList/>
     </React.StrictMode>
 );
 

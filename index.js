940e1e20d59ad54992f5e2b3f2c08953633e5d09
Waltdev29 <waltdev29@gmail.com>
2025-11-04 16:23:58 +0900

    11장 Form(select 태그)

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import NameForm from "./chp11/exam01/NameForm";
+import FruitSelect from "./chp11/exam02/FruitSelect";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <NameForm/>
+        <FruitSelect/>
     </React.StrictMode>
 );
 
